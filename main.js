35f73f3b0fb75d8b874617c437673d527f614516
marine-bre <marine_bretonniere@hotmail.co.uk>
2021-01-31 10:13:56 +0200

    visual changes

--- main.js
+++ main.js
@@ -5,38 +5,62 @@ const home = document.querySelector(".home");
 const about = document.querySelector(".about");
 const keyboard = document.querySelector(".home--img");
 const contact = document.querySelector(".contact");
-
 const project1 = document.querySelector("#project1");
-
-const project1Img1 = document.querySelector(".project--img--multiple").children[0]
-const project1Img2 = document.querySelector(".project--img--multiple").children[1]
-const project1Img3 = document.querySelector(".project--img--multiple").children[2]
 const overlay = document.querySelector(".overlay")
-const project2Img1 = document.querySelector(".project--img--multiple--left").children[0]
-const project2Img2 = document.querySelector(".project--img--multiple--left").children[1]
-const project2Img3 = document.querySelector(".project--img--multiple--left").children[2]
 
+
+AOS.init();
+
+//progress bar
+// const p = document.querySelectorAll(".progress--bubble")
+// console.log(p)
+
+const page = document.querySelectorAll(".page")
 
 window.addEventListener("scroll", (e) => {
-    // about.style.transform = `translateY(-${0.40*window.scrollY}px)`;
-    // // keyboard.style.transform = `translateY(-${0.4*window.scrollY}px)`;
-    // project1.style.transform = `translateY(-${0.53*window.scrollY}px)`
+    if (window.scrollY < 0.7 * window.innerHeight) {
+        // p.forEach(el=>el.classList.remove("progress--active"))
+        // p[0].classList.add("progress--active")
+        page.forEach(el=>el.style.backgroundColor = "#316fb6")
+    }
+    if (window.scrollY > 0.7 * window.innerHeight) {
+        // tlAbout.play()
+        // p.forEach(el=>el.classList.remove("progress--active"))
+        // p[1].classList.add("progress--active")
+        page.forEach(el=>el.style.backgroundColor = "rgb(222, 182, 209)")
+    }
+    if (window.scrollY > 0.8 * window.innerHeight * 2  ) {
+        // tlP1.play()
+        // p.forEach(el=>el.classList.remove("progress--active"))
+        // p[2].classList.add("progress--active")
+        page.forEach(el=>el.style.backgroundColor = "#ADEFD1FF")
+    }
+    if (window.scrollY > 0.8 * window.innerHeight * 3) {
+        // tlP2.play()
+        // p.forEach(el=>el.classList.remove("progress--active"))
+        // p[3].classList.add("progress--active")
+        page.forEach(el=>el.style.backgroundColor = "rgb(222, 182, 209)")
 
-    if (window.scrollY > window.innerHeight) {
-        tlAbout.play()
-        // project1.style.transform = `translateY(-${0.40*window.scrollY}px)`
     }
-    if (window.scrollY > window.innerHeight * 2 - (0.4 * window.scrollY)) {
-        tlP1.play()
+    if (window.scrollY > 0.8 * window.innerHeight * 4.5 ) {
+        // tlP3.play()
+        // p.forEach(el=>el.classList.remove("progress--active"))
+        // p[4].classList.add("progress--active")
+        page.forEach(el=>el.style.backgroundColor = "#316fb6")
+
     }
-    if (window.scrollY > window.innerHeight * 3 - (0.4 * window.scrollY)) {
-        tlP2.play()
+    if (window.scrollY > 0.8 * window.innerHeight * 5.5 ) {
+        // tlP4.play()
+        page.forEach(el=>el.style.backgroundColor = "#ADEFD1FF")
+
+        // p.forEach(el=>el.classList.remove("progress--active"))
+        // p[5].classList.add("progress--active")
     }
-    if (window.scrollY > window.innerHeight * 4 - (0.4 * window.scrollY)) {
-        tlP3.play()
-    }
-    if (window.scrollY > window.innerHeight * 5 - (0.4 * window.scrollY)) {
-        tlP4.play()
+    if (window.scrollY > 0.8 * window.innerHeight * 7 ) {
+        // page.forEach(el=>el.style.backgroundColor = "rgb(222, 182, 209)")
+
+        // p.forEach(el=>el.classList.remove("progress--active"))
+        // p[6].classList.add("progress--active")
     }
 })
 
@@ -77,131 +101,79 @@ tl1.fromTo('.home--text', 1.5, {
         }
     })
 
-let tlAbout = gsap.timeline({ paused: true });
-tlAbout.fromTo("[class*='about--']", 1, {
-    y: "-40%",
-    opacity: "0"
-}, {
-    y: "0",
-    opacity: "1",
-    ease: "power1"
-}).fromTo('.highlight--about', 1.5, {
-    backgroundColor: "transparent",
-}, {
-    backgroundColor: "white",
-    ease: "power1"
-}, "-=0.5")
+// let tlAbout = gsap.timeline({ paused: true });
+// tlAbout.fromTo("[class*='about--']", 1, {
+//     y: "-40%",
+//     opacity: "0"
+// }, {
+//     y: "0",
+//     opacity: "1",
+//     ease: "power1"
+// }).fromTo('.highlight--about', 1.5, {
+//     backgroundColor: "transparent",
+// }, {
+//     backgroundColor: "rgba(255, 255, 255, 0.7)",
+//     ease: "power1"
+// }, "-=0.5")
+
 
 //project 1
 
-let tlP1 = gsap.timeline({ paused: true });
-tlP1.fromTo(".project1--el", 1.5, {
-    y: "-40%",
-    opacity: "0"
-}, {
-    y: "0",
-    opacity: "1",
-    ease: "power1"
-}).fromTo(project1Img1, 1.5, {
-    x: "40%",
-    opacity: "0"
-}, {
-    x: "0",
-    opacity: "1",
-    ease: "power1"
-}).fromTo(project1Img2, 1.5, {
-    x: "40%",
-    opacity: "0"
-}, {
-    x: "0",
-    opacity: "1",
-    ease: "power1"
-}, "-=1").fromTo(project1Img3, 1.5, {
-    x: "40%",
-    opacity: "0"
-}, {
-    x: "0",
-    opacity: "1",
-    ease: "power1"
-}, "-=1").fromTo('.highlight', 1.5, {
-    backgroundColor: "transparent",
-}, {
-    backgroundColor: "white",
-    ease: "power1"
-}, "-=0.5")
+// let tlP1 = gsap.timeline({ paused: true });
+// tlP1.fromTo(".project1--el", 1.5, {
+//     y: "-40%",
+//     opacity: "0"
+// }, {
+//     y: "0",
+//     opacity: "1",
+//     ease: "power1"
+// }).fromTo('.highlight', 1.5, {
+//     backgroundColor: "transparent",
+// }, {
+//     backgroundColor: "rgba(255, 255, 255, 0.7)",
+//     ease: "power1"
+// }, "-=0.5")
+
 
 
 //project2
 
-let tlP2 = gsap.timeline({ paused: true });
-tlP2.fromTo(".project2--el", 1.5, {
-    y: "-40%",
-    opacity: "0"
-}, {
-    y: "0",
-    opacity: "1",
-    ease: "power1"
-}).fromTo(project2Img1, 1.5, {
-    x: "-40%",
-    opacity: "0"
-}, {
-    x: "0",
-    opacity: "1",
-    ease: "power1"
-}).fromTo(project2Img3, 1.5, {
-    x: "-40%",
-    opacity: "0"
-}, {
-    x: "0",
-    opacity: "1",
-    ease: "power1"
-}, "-=1").fromTo(project2Img2, 1.5, {
-    x: "-40%",
-    opacity: "0"
-}, {
-    x: "0",
-    opacity: "1",
-    ease: "power1"
-}, "-=1").fromTo('.highlight2', 1.5, {
-    backgroundColor: "transparent",
-}, {
-    backgroundColor: "white",
-    ease: "power1"
-}, "-=0.5")
+// let tlP2 = gsap.timeline({ paused: true });
+// tlP2.fromTo(".project2--el", 1.5, {
+//     y: "-40%",
+//     opacity: "0"
+// }, {
+//     y: "0",
+//     opacity: "1",
+//     ease: "power1"
+// }).fromTo('.highlight2', 1.5, {
+//     backgroundColor: "transparent",
+// }, {
+//     backgroundColor: "rgba(255, 255, 255, 0.7)",
+//     ease: "power1"
+// }, "-=0.5")
+
+
 
 //project 3
-let tlP3 = gsap.timeline({ paused: true });
-tlP3.fromTo(".project3--el", 1.5, {
-    y: "-40%",
-    opacity: "0"
-}, {
-    y: "0",
-    opacity: "1",
-    ease: "power1"
-})
+// let tlP3 = gsap.timeline({ paused: true });
+// tlP3.fromTo(".project3--el", 1.5, {
+//     y: "-40%",
+//     opacity: "0"
+// }, {
+//     y: "0",
+//     opacity: "1",
+//     ease: "power1"
+// })
 
+// //project 4
 
-
-//project 4
-
-let tlP4 = gsap.timeline({ paused: true });
-tlP4.fromTo(".project4--el", 1.5, {
-    y: "-40%",
-    opacity: "0"
-}, {
-    y: "0",
-    opacity: "1",
-    ease: "power1"
-})
-
-
-links = document.querySelectorAll(".menu--a");
-for (let i = 0; i < links.length; i++) {
-    links[i].addEventListener("mouseover", () => {
-        links[i].style.transform = "scale(2)"
-    })
-    links[i].addEventListener("mouseleave", () => {
-        links[i].style.transform = "scale(1)"
-    })
-}
-
+// let tlP4 = gsap.timeline({ paused: true });
+// tlP4.fromTo(".project4--el", 1.5, {
+//     y: "-40%",
+//     opacity: "0"
+// }, {
+//     y: "0",
+//     opacity: "1",
+//     ease: "power1"
+// })
